dd5006caf7271fa91a4124a821c878ace1211810
SaintJul <favor.julius@gmail.com>
2021-06-18 04:52:19 -0400

    commit tree 5a214c4f19dcf759fef4d6d03ba38048594cce6e

---
+++ src/components/EyesOnMe.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function EyesOnMe(){
+return (
+    <button>
+        onFocus = {console.log("Good!")} onBlur = {console.log("Hey! Eyes on me!")}
+    Eyes on me
+    </button>
+)
+
+}
+export default EyesOnMe
